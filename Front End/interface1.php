7440baa34c445700200b43ced6b1c27be1cdff89
ss11k5 <wenyuan.du@alumni.ubc.ca>
2017-03-31 22:26:07 -0700

    Update interface1.php

--- Front End/interface1.php
+++ Front End/interface1.php
@@ -6,148 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>304 Project Final</title>
     <link rel="stylesheet" href="css/app.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-<?php
 
-//this tells the system that it's no longer just parsing 
-//html; it's now parsing PHP
-
-$success = True; //keep track of errors so it redirects the page only if there are no errors
-$db_conn = OCILogon("ora_a6g0b", "a28558147", "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = dbhost.ugrad.cs.ubc.ca)(PORT = 1522)))(CONNECT_DATA=(SID=ug)))");
-
-function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-	//echo "<br>running ".$cmdstr."<br>";
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
-		// connection handle
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	$r = OCIExecute($statement, OCI_DEFAULT);
-	if (!$r) {
-		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-		echo htmlentities($e['message']);
-		$success = False;
-	} else {
-
-	}
-	return $statement;
-
-}
-
-function executeBoundSQL($cmdstr, $list) {
-	/* Sometimes a same statement will be excuted for severl times, only
-	 the value of variables need to be changed.
-	 In this case you don't need to create the statement several times; 
-	 using bind variables can make the statement be shared and just 
-	 parsed once. This is also very useful in protecting against SQL injection. See example code below for       how this functions is used */
-
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr);
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn);
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	foreach ($list as $tuple) {
-		foreach ($tuple as $bind => $val) {
-			//echo $val;
-			//echo "<br>".$bind."<br>";
-			OCIBindByName($statement, $bind, $val);
-			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-
-		}
-		$r = OCIExecute($statement, OCI_DEFAULT);
-		if (!$r) {
-			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-			$e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
-			echo htmlentities($e['message']);
-			echo "<br>";
-			$success = False;
-		}
-	}
-
-}
-
-function printResult($result) { //prints results from a select statement
-	echo "<br>Got data from table tab1:<br>";
-	echo "<table>";
-	echo "<tr><th>ID</th><th>Name</th></tr>";
-
-	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]" 
-	}
-	echo "</table>";
-
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-// Connect Oracle...
-if ($db_conn) {
-	$username = "gPotts";//$_SESSION['username'];
-	$res = executePlainSQL("SELECT StudentID, StudentName FROM Student WHERE Username = '$username'");
-	$array = oci_fetch_array($res);
-	$studentID = (int)$array[0];
-	$sname = $array[1];
-	//Commit to save changes...
-	OCILogoff($db_conn);
-} else {
-	echo "cannot connect";
-	$e = OCI_Error(); // For OCILogon errors pass no handle
-	echo htmlentities($e['message']);
-}
-
-/* OCILogon() allows you to log onto the Oracle database
-     The three arguments are the username, password, and database
-     You will need to replace "username" and "password" for this to
-     to work. 
-     all strings that start with "$" are variables; they are created
-     implicitly by appearing on the left hand side of an assignment 
-     statement */
-
-/* OCIParse() Prepares Oracle statement for execution
-      The two arguments are the connection and SQL query. */
-/* OCIExecute() executes a previously parsed statement
-      The two arguments are the statement which is a valid OCI
-      statement identifier, and the mode. 
-      default mode is OCI_COMMIT_ON_SUCCESS. Statement is
-      automatically committed after OCIExecute() call when using this
-      mode.
-      Here we use OCI_DEFAULT. Statement is not committed
-      automatically when using this mode */
-
-/* OCI_Fetch_Array() Returns the next row from the result data as an  
-     associative or numeric array, or both.
-     The two arguments are a valid OCI statement identifier, and an 
-     optinal second parameter which can be any combination of the 
-     following constants:
-
-     OCI_BOTH - return an array with both associative and numeric 
-     indices (the same as OCI_ASSOC + OCI_NUM). This is the default 
-     behavior.  
-     OCI_ASSOC - return an associative array (as OCI_Fetch_Assoc() 
-     works).  
-     OCI_NUM - return a numeric array, (as OCI_Fetch_Row() works).  
-     OCI_RETURN_NULLS - create empty elements for the NULL fields.  
-     OCI_RETURN_LOBS - return the value of a LOB of the descriptor.  
-     Default mode is OCI_BOTH.  */
-?>
 <div class="row nav">
     <div class="large-4, medium-4, small-4 columns">
 
@@ -156,34 +18,213 @@ if ($db_conn) {
     </div>
     <div class="large-8, medium-8, small-8 columns">
         <ul>
-            <li><a href="/~a6g0b/index.html" class="nav">Sign out</a></li>
-            <li><a href="/~a6g0b/interface3.php" class="nav">Payment</a></li>
-            <li><a href="/~a6g0b/research.php" class="nav">Research</a></li>
+            <li><a href="/index.html" class="nav">Sign out</a></li>
+            <li><a href="/interface3.html" class="nav">Payment</a></li>
+            <li><a href="/interface4.html" class="nav">Research</a></li>
         </ul>
     </div>
 </div>
 <div class="large-12 columns text-center">
-    <h1>Course information</h1>
-    <h6>The table will be shown below:</h6>
-    <table id = "tblResults"></table>
+    <form method="POST" action="interface1.php">
+        <h1>Course information</h1>
+        <p>Find course with credits:</p><br>
+
+        <p><input type="text" name="credits" size="6"></p>
+        <p><input type="submit" name="submit" id="submit" value="submit"></p>
+        <h6>The table will be shown below:</h6>
+    </form>
+
+
+
 </div>
-<script>
 
 
-    function  generateTable(data) {
-    var tbl_body = document.createElement("tbody");
-    var odd_even = false;
-    console.log("DATA",data);
-    $.each(data,function () {
-        var tbl_row = tbl_body.insertRow();
-        tbl_row.className = odd_even ? "odd" : "even";
-        $.each(this, function (k, v) {
-            var cell = tbl_row.insertCell();
-            cell.appendChild(document.createTextNode(v.toString()));
-        })
-        odd_even = !odd_even;
-    })
-    document.getElementById("tblResults").appendChild(tbl_body);
-}</script>
+
+<table >
+    <style>
+
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+        table{width:100%; border: 2px solid #ddd;
+            text-align: left;}
+        th{color:blue;   font-size: 140%; font-weight: bold;border: 1px solid #ddd;
+            text-align: left; padding: 15px;}
+        td{color:black; font-size: 140%; font-weight:bold; border: 1px solid #ddd;
+            text-align: center; padding: 10px;}
+
+    </style>
+    <thead>
+
+
+    <th>Course ID	</th>
+    <th>Subject	</th>
+    <th>Credits		</th>
+    <th>profID	</th>
+    <th>prof name	</th>
+    <th>office	</th>
+    <th>prof Rating	</th>
+
+
+
+    </thead>
+    <tbody>
+
+
+
+    </tbody>
+</table>
+
+<?php
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_a6g0b", "a28558147", "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = dbhost.ugrad.cs.ubc.ca)(PORT = 1522)))(CONNECT_DATA=(SID=ug)))");
+
+function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+    //echo "<br>running ".$cmdstr."<br>";
+    global $db_conn, $success;
+    $statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the
+        // connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    $r = OCIExecute($statement, OCI_DEFAULT);
+    if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+    } else {
+
+    }
+    return $statement;
+
+}
+
+function executeBoundSQL($cmdstr, $list) {
+    /* Sometimes a same statement will be excuted for severl times, only
+     the value of variables need to be changed.
+     In this case you don't need to create the statement several times;
+     using bind variables can make the statement be shared and just
+     parsed once. This is also very useful in protecting against SQL injection. See example code below for       how this functions is used */
+
+    global $db_conn, $success;
+    $statement = OCIParse($db_conn, $cmdstr);
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn);
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    foreach ($list as $tuple) {
+        foreach ($tuple as $bind => $val) {
+            //echo $val;
+            //echo "<br>".$bind."<br>";
+            OCIBindByName($statement, $bind, $val);
+            unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+
+        }
+        $r = OCIExecute($statement, OCI_DEFAULT);
+        if (!$r) {
+            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
+            echo htmlentities($e['message']);
+            echo "<br>";
+            $success = False;
+        }
+    }
+
+}
+
+function printResult($res1) { //prints results from a select statement
+
+    echo "<table>";
+
+
+    while ($array1 = OCI_Fetch_Array($res1, OCI_BOTH)) {
+        echo "<td>".$array1[0]."</td>";
+        echo "<td>".$array1[1]."</td>";
+        echo "<td>".$array1[2]."</td>";
+        echo "<td>".$array1[4]."</td>";
+        echo "<td>".$array1[5]."</td>";
+        echo "<td>".$array1[6]."</td>";
+        echo "<td>".$array1[7]."</td>";
+    }
+    echo "</table>";
+
+}
+
+// Connect Oracle...
+if ($db_conn) {
+    $t = $_POST['credits'];
+    $int = (int)$t;
+    if ($int == 0){
+        $prof = executePlainSQL("SELECT CourseID FROM Course");}
+    else {
+        $prof = executePlainSQL("SELECT CourseID FROM Course WHERE Course.Credits=$int");
+    }
+    $counter = 0;
+    while ($array = OCI_Fetch_Array($prof, OCI_BOTH)) {
+        $newarray[$counter] = $array[0];
+
+        $counter = $counter + 1;
+    }
+    if ($counter == 0){
+        echo "No courses found!";
+    }
+
+    if (array_key_exists('submit', $_POST)){
+        echo array_key_exists('submit');
+        $i=0;
+
+        while($counter>0)
+        {   $num = $newarray[$i];
+            $res1 = executePlainSQL("SELECT * FROM Course c LEFT OUTER JOIN Professor t ON t.profID = c.profID WHERE c.CourseID='$num'");
+            printResult($res1);
+            $counter=$counter-1;
+            $i=$i+1;
+        }
+
+        OCICommit($db_conn);
+    }
+
+
+    if ($_POST && $success) {
+//        && array_key_exists('submit', $_POST)
+//        //POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
+//        header("location: interface1.php");
+    } else {
+        // Select data...
+        $t = $_POST['credits'];
+        $int = (int)$t;
+        if ($int == 0){
+            $prof = executePlainSQL("SELECT CourseID FROM Course");}
+        else {
+            $prof = executePlainSQL("SELECT CourseID FROM Course WHERE Course.Credits=$int");
+        }
+
+        $counter = 0;
+        while ($array = OCI_Fetch_Array($prof, OCI_BOTH)) {
+            $newarray[$counter] = $array[0];
+
+            $counter = $counter + 1;
+        }
+
+    }
+    //Commit to save changes...
+    OCILogoff($db_conn);
+} else {
+    echo "cannot connect";
+    $e = OCI_Error(); // For OCILogon errors pass no handle
+    echo htmlentities($e['message']);
+}
+?>
 </body>
 </html>
